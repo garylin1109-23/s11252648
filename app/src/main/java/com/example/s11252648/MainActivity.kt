121f4f4807b0d182a7bb7bf342084ddc51e93c03
林庚賢 <garylin9211@gmail.com>
2024-12-09 16:51:26 +0800

    v1_林庚賢-1

--- app/src/main/java/com/example/s11252648/MainActivity.kt
+++ app/src/main/java/com/example/s11252648/MainActivity.kt
@@ -1,16 +1,27 @@
 package com.example.s11252648
 
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateMapOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.s11252648.ui.theme.S11252648Theme
 
 class MainActivity : ComponentActivity() {
@@ -24,6 +35,8 @@ class MainActivity : ComponentActivity() {
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
+
+
                 }
             }
         }
@@ -31,17 +44,50 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting (name: String, modifier: Modifier = Modifier) {
+
+        Column {
+            Modifier.background(Color(0xff95fe95)).fillMaxSize()
+
+
+                Text(
+                text = "遊戲持續時間 0 秒\n"
+
+            )
+
+            Text(
+                text = "您的成績 0 分\n"
+
+            )
+
+
+        }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    S11252648Theme {
-        Greeting("Android")
-    }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
